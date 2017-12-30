e9694bf93bc8fd2cc02a9dff684968ac9cf57c23
kaspernj <kaspernj@gmail.com>
2017-12-30 19:54:15 +0100

    Only open SSH port. Others should be opened through SSH itself (forwarded)

--- run.sh
+++ run.sh
@@ -3,9 +3,6 @@
 docker run -d -P \
   --name my-project \
   --publish 1022:22 \
-  --publish 13000:3000 \
-  --publish 13306:3306 \
-  --publish 15432:5432 \
   --restart=always \
   -v /path/to/shared:/shared \
   rails-development-base
