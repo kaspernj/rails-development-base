21bf0d14085a3181be3fb82720d38d3c1ce053e0
kaspernj <kaspernj@gmail.com>
2018-05-15 17:35:07 +0200

    Worked more on everything

--- build_ubuntu_16_04.sh
+++ build_ubuntu_16_04.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker build -t rails-development-base-ubuntu-16-04 image_ubuntu_16_04/
+docker build -t kaspernj/rails-development-base-ubuntu-16-04 image_ubuntu_16_04/
--- build_ubuntu_17_10.sh
+++ build_ubuntu_17_10.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker build -t rails-development-base-ubuntu-17-10 image_ubuntu_17_10/
+docker build -t kaspernj/rails-development-base-ubuntu-17-10 image_ubuntu_17_10/
