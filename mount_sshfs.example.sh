0aa2118948c3034c17a22d5397f8f8a804e7fa6a
kaspernj <kaspernj@gmail.com>
2018-11-02 11:17:03 +0100

    Changed to expected Development folder

--- mount_sshfs.example.sh
+++ mount_sshfs.example.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-sshfs -o reconnect,noappledouble,ServerAliveInterval=15,ServerAliveCountMax=3,port=1022 dev@container.dev:/home/dev/project /Users/username/Dev/project
+sshfs -o reconnect,noappledouble,ServerAliveInterval=15,ServerAliveCountMax=3,port=1022 dev@container.dev:/home/dev/Development /Users/username/Dev/project
