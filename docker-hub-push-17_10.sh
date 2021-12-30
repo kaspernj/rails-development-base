3d911d3decef4b6f11b97634169be44fce8b7248
kaspernj <kaspernj@gmail.com>
2021-12-30 09:30:50 +0100

    Removed expires 17.10

--- docker-hub-push-17_10.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-docker push kaspernj/rails-development-base-ubuntu-17-10
--- image_ubuntu_17_10/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-echo Starting container
-ruby /shared/scripts/entrypoint.rb > /shared/log/last_startup
-
-echo Starting SSH daemon
-/usr/sbin/sshd -D
