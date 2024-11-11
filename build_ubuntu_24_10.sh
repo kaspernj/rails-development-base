901c05d5ce23ab6ea4b26485218b59851cc14df9
kaspernj <k@spernj.org>
2024-11-11 12:10:33 +0100

    Added 24.10

--- build_ubuntu_24_10.sh
+++ build_ubuntu_24_10.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker build -t kaspernj/rails-development-base-ubuntu-22-04 image_ubuntu_22_04/
+docker build -t kaspernj/rails-development-base-ubuntu-24-10 image_ubuntu_24_10/
--- shared/scripts/entrypoint.rb
+++ shared/scripts/entrypoint.rb
@@ -4,32 +4,32 @@ require "fileutils"
 
 FileUtils.copy("/shared/dev_sudo_access", "/etc/sudoers.d/90-dev-user")
 
-unless File.exists?("/home/dev/.ssh")
+unless File.exist?("/home/dev/.ssh")
   puts "Preparing home dir"
   FileUtils.chown("dev", "dev", "/home/dev")
   system("find", "/home/dev-sample/", "-mindepth", "1", "-maxdepth", "1", "-exec", "cp", "-rp", "{}", "/home/dev/", ";")
 end
 
-unless File.exists?("/shared/ssh/authorized_keys")
+unless File.exist?("/shared/ssh/authorized_keys")
   puts "Copying sample authorized_keys since none exists"
   FileUtils.copy("/shared/ssh/authorized_keys.example", "/shared/ssh/authorized_keys")
 end
 
 puts "Fixing SSH permissions"
 FileUtils.chown("dev", "dev", ["/home/dev/.ssh/authorized_keys", "/home/dev/.ssh/config"])
-File.chmod(0600, "/home/dev/.ssh/authorized_keys") if File.exists?("/home/dev/.ssh/authorized_keys")
-File.chmod(0600, "/home/dev/.ssh/config") if File.exists?("/home/dev/.ssh/config")
+File.chmod(0600, "/home/dev/.ssh/authorized_keys") if File.exist?("/home/dev/.ssh/authorized_keys")
+File.chmod(0600, "/home/dev/.ssh/config") if File.exist?("/home/dev/.ssh/config")
 
 puts "Installing SSHD config"
 
-unless File.exists?("/shared/ssh/sshd_config")
+unless File.exist?("/shared/ssh/sshd_config")
   puts "Copying sample SSHD config since none exists"
   FileUtils.copy("/etc/ssh/sshd_config.original", "/shared/ssh/sshd_config")
 end
 
-File.unlink("/etc/ssh/sshd_config") if File.exists?("/etc/ssh/sshd_config")
+File.unlink("/etc/ssh/sshd_config") if File.exist?("/etc/ssh/sshd_config")
 
-unless File.exists?("/shared/ssh/config")
+unless File.exist?("/shared/ssh/config")
   puts "Copying sample SSH config since none exists"
   FileUtils.copy("/shared/ssh/config.example", "/shared/ssh/config")
 end
@@ -37,18 +37,18 @@ end
 shared_sshd_file_path = "/shared/ssh/sshd_config"
 etc_sshd_file_path = "/etc/ssh/sshd_config"
 
-if !File.exists?(etc_sshd_file_path) || File.size(shared_sshd_file_path) != File.size(etc_sshd_file_path)
+if !File.exist?(etc_sshd_file_path) || File.size(shared_sshd_file_path) != File.size(etc_sshd_file_path)
   puts "Copying #{shared_sshd_file_path} to #{etc_sshd_file_path}"
   FileUtils.copy(shared_sshd_file_path, etc_sshd_file_path)
 end
 
 compose_environment_file_path = "/shared/profile"
 
-if File.exists?(compose_environment_file_path)
+if File.exist?(compose_environment_file_path)
   puts "Installing custom profile"
 
   profile_path = "/home/dev/.profile"
-  FileUtils.touch(profile_path) unless File.exists?(profile_path)
+  FileUtils.touch(profile_path) unless File.exist?(profile_path)
   profile_content = File.read(profile_path)
 
   unless profile_content.include?(". #{compose_environment_file_path}")
