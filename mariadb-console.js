0544f5d56c1643850ca266346644d4a5ed912937
kaspernj <kasper@diestoeckels.de>
2026-03-13 18:45:43 +0100

    More packages, fixed mariadb console script, added docker server and registry to example docker compose config

--- mariadb-console.js
+++ mariadb-console.js
@@ -6,8 +6,14 @@ import yaml from "js-yaml"
 
 const dockerComposeContent = await fs.readFile("./docker-compose.yml")
 const dockerCompose = yaml.load(dockerComposeContent)
-const rootPassword = dockerCompose.services.mariadb.environment.MYSQL_ROOT_PASSWORD
+const mariadbEnvironment = dockerCompose?.services?.mariadb?.environment
+const rootPassword = mariadbEnvironment?.MARIADB_ROOT_PASSWORD || mariadbEnvironment?.MYSQL_ROOT_PASSWORD
 const thisDirName = process.cwd().split("/").pop()
+
+if (!rootPassword) {
+  throw new Error("Could not detect MariaDB root password from docker-compose.yml. Expected MARIADB_ROOT_PASSWORD or MYSQL_ROOT_PASSWORD.")
+}
+
 const command = `/usr/bin/mariadb -u root -p${rootPassword}`
 
 spawn("docker", ["exec", "-it", `${thisDirName}-mariadb-1`, "/bin/bash", "-c", command], {
